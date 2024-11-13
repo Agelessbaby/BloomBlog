5261cbc3a69de1174bd7c6b950df08d987cc597a
liu wenjie <205902584@qq.com>
2024-11-13 15:47:51 +0800

    change some error

--- cmd/relation/client_for_test/main.go
+++ cmd/relation/client_for_test/main.go
@@ -9,13 +9,19 @@ import (
 )
 
 func main() {
-	client, err := relationsrv.NewClient("github.com/Agelessbaby/BloomBlog.relation", client.WithHostPorts("0.0.0.0:8087"))
+	client, err := relationsrv.NewClient("BloomBlogRelationServer", client.WithHostPorts("0.0.0.0:8087"))
 	if err != nil {
 		panic(err)
 	}
-	resp, err := client.RelationFollowerList(context.TODO(), &relation.BloomblogRelationFollowerListRequest{
-		UserId: 1,
-		Token:  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIiLCJpc3MiOiJCbG9vbUJMb2ciLCJhdWQiOiIiLCJzdWIiOiIiLCJpYXQiOjE3MzEzMTExNzAsIm5iZiI6MCwiZXhwIjoxNzMyMDAyMzcwLCJ1ZCI6eyJ1c2VyaWQiOjN9fQ.2mwVT7xI46NS_dvTZZOvcslbQ4X6z82oMWh07UQ9_tY",
+	//resp, err := client.RelationFollowerList(context.TODO(), &relation.BloomblogRelationFollowerListRequest{
+	//	UserId: 1,
+	//	Token:  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIiLCJpc3MiOiJCbG9vbUJMb2ciLCJhdWQiOiIiLCJzdWIiOiIiLCJpYXQiOjE3MzEzMTExNzAsIm5iZiI6MCwiZXhwIjoxNzMyMDAyMzcwLCJ1ZCI6eyJ1c2VyaWQiOjN9fQ.2mwVT7xI46NS_dvTZZOvcslbQ4X6z82oMWh07UQ9_tY",
+	//})
+	resp, err := client.RelationAction(context.TODO(), &relation.BloomblogRelationActionRequest{
+		UserId:     0,
+		Token:      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIiLCJpc3MiOiJCbG9vbUJMb2ciLCJhdWQiOiIiLCJzdWIiOiIiLCJpYXQiOjE3MzEzMTExNzAsIm5iZiI6MCwiZXhwIjoxNzMyMDAyMzcwLCJ1ZCI6eyJ1c2VyaWQiOjN9fQ.2mwVT7xI46NS_dvTZZOvcslbQ4X6z82oMWh07UQ9_tY",
+		ToUserId:   3,
+		ActionType: 1,
 	})
 	if err != nil {
 		panic(err)
